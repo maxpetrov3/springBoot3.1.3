0c463f522a8b4156fabcdf447e40c1b1aa79199f
maxpetrov3 <max.com.07@mail.ru>
2021-10-25 23:28:58 +0400

    main panel and a half of admin is done

--- src/main/java/sb241/sb241/controller/AdminController.java
+++ src/main/java/sb241/sb241/controller/AdminController.java
@@ -6,7 +6,6 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import sb241.sb241.service.UserService;
 
 @Controller
@@ -18,7 +17,7 @@ public class AdminController {
 	@GetMapping(value = "/admin")
 	public String getUsers(ModelMap model) {
 		model.addAttribute("users", userService.getAllUsers());
-		return "admin";
+		return "mainPanel";
 	}
 
 	@PostMapping(value = "/delete")
